0ef6b473133dc85815931ba804f6eea3e2460020
andre goncalves <startrule@gmail.com>
2019-08-31 11:47:30 +0100

    added accessible svg button

---
+++ src/components/common/ImgButton.vue
@@ -0,0 +1,50 @@
+<template>
+    <button
+    class="image-button"
+    @click="emitClicked"
+    :class="classes"
+    :hidden="hidden">
+        <svg-loader
+        :svgName="svgName"
+        :alt="alt">
+        </svg-loader>
+    </button>
+</template>
+
+<script>
+/* NOTE: There is no place for text since this button is a image only button
+made to easier accessability in the page, if we need a button with text
+in the future it will be better to just make a new component
+*/
+import SvgLoader from '@/components/common/SvgLoader'
+export default {
+	props: {
+		classes: String,
+		hidden: { /* by default we want it to be visible */
+			type: Boolean,
+			default: () => { return false }
+		},
+		svgName: String,
+		alt: String
+	},
+	components: {
+		'svg-loader': SvgLoader
+	},
+	methods: {
+		emitClicked (ev) {
+			console.log(ev)
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.image-button {
+    background-color: transparent;
+    outline: none;
+    border: none;
+    &:focus {
+        box-shadow: 0 0 3px 1px #859EFF;
+    }
+}
+</style>
