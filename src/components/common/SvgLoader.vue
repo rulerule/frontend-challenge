61bc07134e92d753e5ef3c5bd1cb59ade91c153d
andre goncalves <startrule@gmail.com>
2019-08-31 11:47:21 +0100

    added svg loader

---
+++ src/components/common/SvgLoader.vue
@@ -0,0 +1,21 @@
+<template>
+    <img class="svg-icon" :src="require(`@/assets/icons/${svgName}.svg`)" :alt="alt" :title="title">
+</template>
+
+<script>
+export default {
+	props: {
+		svgName: String,
+		alt: String,
+		title: String
+	}
+}
+</script>
+
+<style lang="scss">
+.svg-icon {
+    &:hover {
+        cursor:pointer;
+    }
+}
+</style>
