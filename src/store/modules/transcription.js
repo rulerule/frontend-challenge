e907f098be3d90cd44f05bb2eb178fcc8c1eb07c
andre goncalves <startrule@gmail.com>
2019-08-31 18:30:25 +0100

    added actions, getters and mutations for fetch process

--- src/store/modules/transcription.js
+++ src/store/modules/transcription.js
@@ -1,7 +1,34 @@
-const state = {}
-const getters = {}
-const mutations = {}
-const actions = {}
+const state = {
+	transcriptionsData: []
+}
+const getters = {
+	getTranscriptionsData: state => {
+		return state.transcriptionsData
+	}
+}
+
+const mutations = {
+	fetchTranscriptionsData: (state, payload) => {
+		state.transcriptionsData = payload
+	}
+}
+
+const actions = {
+	fetchTranscriptionsData: (context, payload) => {
+		const request = axios({
+			method: 'GET'
+		})
+		request.then(res => {
+			context.commit('fetchTranscriptionsData', res.data)
+		})
+	},
+	/* TODO */
+	uploadTranscriptionsData: (context, payload) => {
+		const request = axios({
+			method: 'POST'
+		})
+	}
+}
 
 export default {
 	state,
