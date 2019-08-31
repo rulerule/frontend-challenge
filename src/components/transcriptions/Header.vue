0e364a3fe7fa613e369eeb3cacb5f972b03e0340
andre goncalves <startrule@gmail.com>
2019-08-31 16:30:32 +0100

    updated transcription header

--- src/components/transcriptions/Header.vue
+++ src/components/transcriptions/Header.vue
@@ -1,18 +1,18 @@
 <template>
-    <header>
+    <header class="transcription-header-wrapper">
         <!-- header content -->
-        <div class="center-element add-max-width-1024">
-            <title> teste title </title>
+        <div class="transcription-header-content center-element add-max-width-80">
+            <title class="header-title"> {{$t('headerTitle')}} </title>
             <div class="buttons-container">
-                <!-- get data -->
-                <image-button
-                svgName="fetch-document"
-                alt="fetch information">
-                </image-button>
                 <!-- upload data -->
                 <image-button
                 svgName="upload"
                 alt="upload information">
+                </image-button>
+                <!-- get data -->
+                <image-button
+                svgName="fetch-document"
+                alt="fetch information">
                 </image-button>
             </div>
         </div>
@@ -32,9 +32,29 @@ export default {
 </script>
 
 <style lang="scss">
-header {
-    &:focus {
-        background-color:red;
+.transcription-header-wrapper {
+    width:100%;
+    height: 60px;
+    background-color:white;
+    border-top-left-radius:6px;
+    border-top-right-radius:6px;
+    border-bottom:1px solid gray;
+    box-shadow: 0 4px 2px -2px gray;
+}
+.transcription-header-content {
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:space-between;
+    .header-title {
+        text-transform: capitalize;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 500;
+        font-size:1.8rem;
+        color: #414c5E;
     }
 }
+
 </style>
