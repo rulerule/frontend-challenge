dc1165db738840d43a627d32eebb3259d6597cd5
andre goncalves <startrule@gmail.com>
2019-08-31 16:29:23 +0100

    updated transcriptions

---
+++ src/components/transcriptions/Base.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="transcription-wrapper">
+        <transcription-header></transcription-header>
+        <transcription-content></transcription-content>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/transcriptions/Header'
+import Content from '@/components/transcriptions/Content'
+export default {
+	components: {
+		'transcription-header': Header,
+		'transcription-content': Content
+	}
+}
+</script>
+
+<style lang="scss">
+.transcription-wrapper {
+    width:100%;
+    height:100%;
+    border-radius:6px;
+    overflow:hidden;
+    &:hover {
+        -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.25);
+        -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.25);
+        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.25);
+    }
+}
+</style>
---
+++ src/components/transcriptions/Content.vue
@@ -0,0 +1,28 @@
+<template>
+    <main class="transcription-content-wrapper center-element add-max-width-80">
+        <transcription-table></transcription-table>
+        <!-- <image-button></image-button> -->
+    </main>
+</template>
+
+<script>
+import Table from '@/components/transcriptions/Table'
+// import ImageButton from '@/components/common/ImgButton'
+export default {
+	data () {
+		return {}
+	},
+	components: {
+		'transcription-table': Table
+		// 'image-button': ImageButton
+	}
+}
+</script>
+
+<style lang="scss">
+.transcription-content-wrapper {
+    width:100%;
+    box-sizing:border-box;
+    padding-top:20px;
+}
+</style>
