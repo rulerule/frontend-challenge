3db3b0c41a6718218380fbddccb0177489558fae
andre goncalves <startrule@gmail.com>
2019-09-01 17:16:18 +0100

    added a very basic flow of testing

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,42 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
+describe('basic flow', () => {
+	it('Create new entry with the add new from empty file', () => {
+		cy.visit('/')
+		cy.get('#addNewButton')
+			.should('be.visible')
+			.click()
+		cy.get('.transcription-table-row')
+			.should('be.visible')
+			.should('have.length', 1)
+	})
+	it('Create entries by downloading information', () => {
+		cy.visit('/')
+		cy.get('#downloadData')
+			.should('be.visible')
+			.click()
+		cy.get('.transcription-table-row')
+			.should('be.visible')
+			.should('have.length', 3)
+	})
+	it('Create entries with add new after downloading', () => {
+		cy.visit('/')
+		cy.get('#downloadData')
+			.click()
+		cy.get('#addNewButton')
+			.click()
+		cy.get('.transcription-table-row')
+			.should('have.length', 4)
+	})
+	it('deletes all entries', () => {
+		cy.visit('/')
+		cy.get('#downloadData')
+			.click()
+		cy.get('.delete-button')
+			.each(($el) => {
+				$el.click()
+			})
+		cy.get('.transcription-table-row')
+			.should('have.length', 0)
+	})
 })
